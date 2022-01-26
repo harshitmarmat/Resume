ebe99f19d03259309e0bedb642ef10fe08704539
harshitmarmat <you@example.com>
2022-01-27 03:04:20 +0530

    toggle Functionality Done

--- src/layout/sidebar/Sidebar.js
+++ src/layout/sidebar/Sidebar.js
@@ -1,10 +1,21 @@
 import classes from './Sidebar.module.css'
 import toggle from '../../assets/image/next.png'
+import { useState } from 'react'
 
 const Sidebar = () => {
+    const [isWideBar, setIsWideBar] = useState(false);
+
+    const toggleHandler = ()=>{
+        console.log(isWideBar);
+        setIsWideBar(!isWideBar);
+    }
+
+    const toggleClass = isWideBar?classes.wideContainer:classes.container;
+    const toggleIcon = isWideBar?classes.closeSideIcon:classes.sideicon;
+
     return(
-        <div className={classes.container}>
-            <img className={classes.sideicon} src={toggle}/>
+        <div className={toggleClass}>
+            <img onClick={toggleHandler} className={toggleIcon} src={toggle} alt='toggle button'/>
             <div className={classes['content-box']}>
             </div>
         </div>
